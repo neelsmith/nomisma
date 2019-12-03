6ed384b20f14fc0c39cb63e7b684cd1ce2fc7165
Neel Smith <dnsmith.neel@gmail.com>
2019-12-03 17:36:05 -0500

    Rename

--- jvm/src/test/scala/GenericIssuesCatalogSourceSpec.scala
+++ jvm/src/test/scala/GenericIssuesCatalogSourceSpec.scala
@@ -6,16 +6,16 @@ import edu.holycross.shot.histoutils._
 class GenericIssuesCatalogSpec extends FlatSpec {
 
 
-  val ocre = OcreSource.fromFile("jvm/src/test/resources/ocre-sample10.cex")
+  val genericOcre = GenericIssuesCatalogSource.fromFile("jvm/src/test/resources/ocre-sample10.cex")
 
-  "An Ocre" should "create a Histogram for a given property" in  {
-    val metalHisto : Histogram[String] = ocre.histogram("material")
+  "A GenericIssuesCatalog" should "create a Histogram for a given property" in  {
+    val metalHisto : Histogram[String] = genericOcre.histogram("material")
     val expectedMetals = 3
     assert(metalHisto.size == expectedMetals)
   }
 
   it should "create an empty Histogram with a warning if the property name is bad" in {
-    val bogusHisto: Histogram[String] = ocre.histogram("bad property name")
+    val bogusHisto: Histogram[String] = genericOcre.histogram("bad property name")
     assert(bogusHisto.size == 0)
   }
 }
